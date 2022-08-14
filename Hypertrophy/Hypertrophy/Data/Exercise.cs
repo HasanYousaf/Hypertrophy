9780869fbafc734e49d2646432f01a7797f6180e
HasanYousaf <91735835+HasanYousaf@users.noreply.github.com>
2022-08-14 18:44:53 -0400

    Exercise class complete

---
+++ Hypertrophy/Hypertrophy/Data/Exercise.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hypertrophy.Data
+{
+        public enum ExerciseCategory
+        {
+            Barbell,
+            Dumbbell,
+            Machine,
+            Cable,
+            Weighted,
+            Bodyweight,
+            AssistedBodyweight,
+            RepsOnly,
+            Cardio,
+            Duration,
+            Other
+        }
+
+        public enum BodyPart
+        {
+            Core,
+            Arms,
+            Back,
+            Chest,
+            Legs,
+            Shoulders,
+            Olympic,
+            FullBody,
+            Cardio,
+            Other
+        }
+    public class Exercise
+    {
+        public BodyPart BodyPart { get; set; }
+        public ExerciseCategory Category { get; set; }
+        public string ExerciseName { get; set; }
+    }
+}
---
+++ Hypertrophy/Hypertrophy/Data/ExerciseRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hypertrophy.Data
+{
+    public class ExerciseRepository
+    {
+        public List<Exercise> GetExercises()
+        {
+            return new List<Exercise>()
+            {
+                new Exercise
+                {
+                    ExerciseName = "Aerobics",
+                    BodyPart = BodyPart.Cardio,
+                    Category = ExerciseCategory.Cardio
+                },
+                new Exercise
+                {
+                    ExerciseName = "Arnold Press",
+                    BodyPart = BodyPart.Shoulders,
+                    Category = ExerciseCategory.Dumbbell
+                },
+                new Exercise
+                {
+                    ExerciseName = "Back Extension",
+                    BodyPart = BodyPart.Back,
+                    Category = ExerciseCategory.Bodyweight
+                },
+                new Exercise
+                {
+                    ExerciseName = "Bench Dip",
+                    BodyPart = BodyPart.Arms,
+                    Category = ExerciseCategory.Bodyweight
+                },
+                new Exercise
+                {
+                    ExerciseName = "Bench Press",
+                    BodyPart = BodyPart.Chest,
+                    Category = ExerciseCategory.Barbell
+                },
+                new Exercise
+                {
+                    ExerciseName = "Close-Grip Bench Press",
+                    BodyPart = BodyPart.Chest,
+                    Category = ExerciseCategory.Barbell
+                },
+            };
+        }
+    }
+}
